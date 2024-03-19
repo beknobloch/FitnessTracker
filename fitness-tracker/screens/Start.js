b056cf1e26ef95c19bcbcdfdf507e79c7c2d4e38
omar <elkhafif.omar96@gmail.com>
2024-03-19 15:02:28 -0400

    figured out immplementation for images on the app

--- fitness-tracker/screens/Start.js
+++ fitness-tracker/screens/Start.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import ExampleQuery from '../components/ExampleQuery';
 import { Button } from 'react-native-elements';
 
 function Start(props) {
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      {/*Adding an image requires defining a source and definig its style*/}
+      <Image source={require('../assets/StartPageIcon.png')} style={styles.image}resizeMode="cover" />
       <Text>Welcome to our Fitness Tracker!</Text>
       <Button style={styles.button} title='Log in' onPress={() => props.navigation.navigate('Login')}/>
       <Button style={styles.button} title='Sign up' onPress={() => props.navigation.navigate('Signup')}/>
@@ -25,6 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300, // Set the width as needed
+    height: 200, // Set the height as needed
+    marginBottom: 20, // Adjust the margin as needed
+},
   button: {
     paddingVertical: 5,
   }
